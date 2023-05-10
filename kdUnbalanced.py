83bad3477d94d493e560e9659db3c78dafe6e763
Meet <meetp272003@gmail.com>
2023-05-11 01:57:18 +0530

    Pracs

--- kdUnbalanced.py
+++ kdUnbalanced.py
@@ -1,46 +1,42 @@
 import numpy as np
-class Node:
-    def __init__(self, point, axis, left=None, right=None):
-        self.point = point
-        self.axis = axis
+class KDNode:
+    def __init__(self, data, depth=0, left=None, right=None):
+        self.data= data
+        self.depth = depth
         self.left = left
         self.right = right
-class KDTree:
-    def __init__(self, points):
-        self.k = points.shape[1]
-        self.root = self.build_kdtree(points)
-    def build_kdtree(self, points, depth=0):
-        n = points.shape[0]
-        if n <= 0:
-            return None
-        axis = depth % self.k
-        sorted_points = points[points[:, axis].argsort()]
-        mid = n // 2
-        return Node(sorted_points[mid], axis,
-            self.build_kdtree(sorted_points[:mid], depth + 1),
-            self.build_kdtree(sorted_points[mid + 1:], depth + 1))
-    def insert(self, point):
-        def _insert(root, point, depth=0):
-            if root is None:
-                return Node(point, depth % self.k)
-            axis = root.axis
-            if point[axis] < root.point[axis]:
-                root.left = _insert(root.left, point, depth + 1)
-            else:
-                root.right = _insert(root.right, point, depth + 1)
-            return root
-        self.root = _insert(self.root, point)
-        
-    def print_kdtree(self, root, level=0):
-        if root is not None:
-            print(" " * level, root.point)
-            self.print_kdtree(root.left, level + 1)
-            self.print_kdtree(root.right, level + 1)
 
-points = np.array([[6,2], [7,1], [2,9], [3,6], [4,8], [8,4], [5,3], [1,5], [9,5]])
-tree = KDTree(points)
+def insert(node,point):
+    if node is None:
+        return KDNode(point)
+    
+    dim=node.depth%2 
+
+    if point[dim] < node.data[dim]:
+        node.left=insert(node.left,point)
+        node.left.depth=node.depth+1
+    else:
+        node.right=insert(node.right,point)
+        node.right.depth=node.depth+1
+        
+    return node
+
+def inorder(node):
+    if node is None:
+      return
+    inorder(node.left)
+    print(f"({','.join(map(str,node.data))}) ",end="")
+    inorder(node.right)
+
+unbalanced_points = np.array([[6, 2], [7, 1], [2, 9], [3, 6], [4, 8], [8, 4], [5, 3], [1, 5], [9, 5]])
+unbalanced_root = KDNode(unbalanced_points[0])
+
+for point in unbalanced_points[1:]: 
+  insert(unbalanced_root, point)
+  
 print("Initial tree:")
-tree.print_kdtree(tree.root)
-tree.insert(np.array([3,5]))
-print("After insertion of point (3,5):")
-tree.print_kdtree(tree.root)
+# print("Unbalanced KD-Tree (inorder traversal):") 
+inorder(unbalanced_root)
+insert(unbalanced_root, [3,5])
+print("\nAfter insertion of point (3,5):")
+inorder(unbalanced_root)
